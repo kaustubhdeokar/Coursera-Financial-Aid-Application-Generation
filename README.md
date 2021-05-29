84858de756ea192c2da928165b48f14ba662838b
kaustubhdeokar <kaustubhd9@gmail.com>
2021-05-29 11:44:49 +0530

    Errors rectified.

--- README.md
+++ README.md
@@ -5,9 +5,6 @@ To generate the script:-
  1. Run the manipulateApplication.py file by <br>
     `python3 manipulateApplication.py`
  2. Enter your github handle.
- 3. Choose the courseraApp file 
- 4. That's it !!!.
-
- `courseraApp` file will contain the coursera financial Aid application. 
-
-### If you want to use this again, copy the text in refresh file in courseraApp file and follow the steps again.
+ 3. Choose the courseraApp file in the resources folder.
+ 4. Enter the field for which you want to generate the application. (Ex. ML, Data Science)   
+ 5. That's it!, the application text file will contain the necessary application.
--- manipulateApplication.py
+++ manipulateApplication.py
@@ -2,32 +2,32 @@ import traceback
 import tkinter as tk
 from tkinter import filedialog
 
+
 def select_file_path():
     root = tk.Tk()
     root.withdraw()
     return filedialog.askopenfilename()
 
-def overwrite_file_content(content,topic,github_handle):
-    content = content.replace("____",topic)
-    content = content.replace("<github-handle>",github_handle)
-    return content
 
-    
-try:
-    github_handle = input('enter your github handle')
-    path = select_file_path()
-    file_read = open(path,'r')
-    orig_content = file_read.read()
-    file_read.close()
-    file_write = open(path,'w')
-    topic = input('Enter the area you wish to apply for')
-    content = overwrite_file_content(orig_content,topic,github_handle)
-    file_write.write(content)
-    file_write.close()
-    print("If you want to use this again, copy the text in refresh file in courseraApp file")
-
-except Exception:
-    print('Check exception occured')
-    print(traceback.print_exc())
+def overwrite_file_content(file_content, field, handle):
+    file_content = file_content.replace("____", field)
+    file_content = file_content.replace("<github-handle>", handle)
+    return file_content
 
 
+if __name__ == "__main__":
+    try:
+        github_handle = input('enter your github handle: ')
+        path = select_file_path()
+        file_read = open(path, 'r')
+        orig_content = file_read.read()
+        file_read.close()
+        file_write = open('application', 'w')
+        topic = input('Enter the area you wish to apply for: ')
+        content = overwrite_file_content(orig_content, topic, github_handle)
+        file_write.write(content)
+        file_write.close()
+        print("Your script can be found in application text file")
+
+    except FileNotFoundError:
+        print(traceback.print_exc())
